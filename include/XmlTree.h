ebf2eea4fe46f880e9ff5faf99401c2e48262189
Daniel Nilsson <daniel.nilsson.9@gmail.com>
2018-01-18 18:06:50 +0100

    added license to source file

--- include/XmlTree.h
+++ include/XmlTree.h
@@ -1,3 +1,12 @@
+/* 
+ * XmlTree.h
+ *
+ * Copyright (C) 2017 Daniel Nilsson
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 #pragma once
 #include <tinyxml2.h>
 
